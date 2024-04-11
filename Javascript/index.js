e6a0a83af343fe590b1a9b4cf8c0b285a030a8f4
riveraborgesj <riveraborgesj@gmail.com>
2024-04-11 17:49:15 -0400

    added moment
    to add real time and dates from timezones and then a function to update every second

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,34 @@
-alert("Hello");
+function updateTime() {
+  //New York
+  let newyorkDateElement = document.querySelector("#new-york .date");
+  let newyorkTimeElement = document.querySelector("#new-york .time");
+  newyorkDateElement.innerHTML = moment()
+    .tz("America / New_York")
+    .format("MMMM Do YYYY");
+  newyorkTimeElement.innerHTML = `${moment()
+    .tz("America / New_York")
+    .format("h:m:ss")} <small>${moment().format("A")}</small>`;
+
+  //Houston
+  let houstonDateElement = document.querySelector("#houston .date");
+  let houstonTimeElement = document.querySelector("#houston .time");
+  houstonDateElement.innerHTML = moment()
+    .tz("US/Central")
+    .format("MMMM Do YYYY");
+  houstonTimeElement.innerHTML = `${moment()
+    .tz("US/Central")
+    .format("h:m:ss")} <small>${moment().format("A")}</small>`;
+
+  //Chicago
+  let chicagoDateElement = document.querySelector("#chicago .date");
+  let chicagoTimeElement = document.querySelector("#chicago .time");
+  chicagoDateElement.innerHTML = moment()
+    .tz("America/Chicago")
+    .format("MMMM Do YYYY");
+  chicagoTimeElement.innerHTML = `${moment()
+    .tz("America/Chicago")
+    .format("h:m:ss")} <small>${moment().format("A")}</small>`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
